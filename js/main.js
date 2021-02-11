b70d3cf4c0b3380e64d6f3a99b4c32c5774c22bd
gustavo.domingues <gustavo.domingues-ext@viavarejo.com.br>
2021-02-11 11:38:13 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function calcular() {
+
+    var nome = document.getElementById('nome').value
+    var custoFixo = parseInt(document.getElementById('custoFixo').value)
+    var salarioReal = custoFixo * 3
+    
+    if(nome == ''){
+        nome = 'Visitante'
+    }
+
+    if(Number.isNaN(custoFixo)){
+        custoFixo = 0
+    }
+
+    if(Number.isNaN(salarioReal)){
+        salarioReal = 0
+    }
+
+    console.log(nome, custoFixo, salarioReal)
+
+    salarioReal = salarioReal.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+
+
+    document.getElementById('infosNome').innerHTML = `Olá, ${nome}, </br>`
+    document.getElementById('infosCustoMensal').innerHTML = `seu salário mensal deveria ser ${salarioReal}`
+
+}
+
+function limpar() {
+    document.location.reload(true)
+}
