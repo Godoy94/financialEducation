ccb7ea202bf659761b07793dd138cc0837b17cdb
Opah <mac@MacBook-Pro---000718.local>
2024-05-06 16:57:32 -0700

    refatorando

--- js/main.js
+++ js/main.js
@@ -1,34 +1,36 @@
-function calcular() {
-
-    var nome = document.getElementById('nome').value
-    var custoFixo = parseFloat(document.getElementById('custoFixo').value)
-    var salarioReal = custoFixo * 3
+function calculate() {
+    var nome = document.getElementById('nome').value;
+    var custoFixo = parseFloat(document.getElementById('custoFixo').value);
+    var salarioReal = custoFixo * 3;
     
     if(nome == ''){
-        nome = 'Visitante'
+        nome = 'Visitante';
     }
 
     if(Number.isNaN(custoFixo)){
-        custoFixo = 0
+        custoFixo = 0;
     }
 
     if(Number.isNaN(salarioReal)){
-        salarioReal = 0
+        salarioReal = 0;
     }
-
-    console.log(nome, custoFixo, salarioReal)
 
     salarioReal = salarioReal.toLocaleString('pt-BR', {
         style: 'currency',
         currency: 'BRL'
-    })
-
+    });
 
     document.getElementById('infosNome').innerHTML = `Olá, ${nome}, </br>`
     document.getElementById('infosCustoMensal').innerHTML = `seu salário mensal deveria ser ${salarioReal}`
 
+    resetInput();
 }
 
-function limpar() {
-    document.location.reload(true)
+function resetInput(){
+    document.getElementById('nome').value = '';
+    document.getElementById('custoFixo').value = '';
+}
+
+function limpar()   {
+    window.location.reload();
 }
